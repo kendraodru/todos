623d9a871e0a0934d95397f7ac8fed2d2c3dae4f
Kendra <kmodrunia@ucdavis.edu>
2020-05-22 08:10:57 -0700

    db mirgation of todos

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_145645) do
+ActiveRecord::Schema.define(version: 2020_05_22_150211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.integer "user_id", null: false
+    t.boolean "done", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
